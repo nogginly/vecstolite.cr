c3686e66b5a5985d28edd55e8f1f43a3314f6632
nogginly <nogginly@icloud.com>
2026-05-15 17:11:05 -0400

    Add wiretap.cr as dev dependency for use with tests

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,8 +1,16 @@
 require "spectator"
+require "wiretap"
 
 require "../src/vecstolite"
 
 Spectator.configure do |config|
   config.fail_blank # Fail on no tests.
   config.randomize  # Randomize test order.
+end
+
+Wiretap.configure do |c|
+  c.transcript_dir = "spec/fixtures/transcripts"
+
+  # If in CI, don't record, fail if not found
+  c.record_mode = ENV["CI"]? ? :none : :once
 end
